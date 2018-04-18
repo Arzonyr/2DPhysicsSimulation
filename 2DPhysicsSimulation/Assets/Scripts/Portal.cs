9506121a73f72eb89b5106ed22862dedccd593a2
Lasse Diedirch <lasse-d@freenet.de>
2018-04-18 14:02:49 +0200

    Added Portal

---
+++ 2DPhysicsSimulation/Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public Rigidbody2D shotRgbd;
+    public Transform TeleportTo;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>() == shotRgbd)
+        {
+            shotRgbd.transform.position = TeleportTo.position;
+        }
+    }
+
+
+}
