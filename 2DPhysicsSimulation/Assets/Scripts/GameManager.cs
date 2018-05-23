dd3c000457e3d38518ee7361c4684837746afded
Lasse Diedirch <lasse-d@freenet.de>
2018-05-23 14:08:06 +0200

    Added Level 01/02/03/04/MainMenu

--- 2DPhysicsSimulation/Assets/Scripts/GameManager.cs
+++ 2DPhysicsSimulation/Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    public string SceneToLoad;
+    
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (GameObject.FindGameObjectWithTag("Enemy") != null)
+            return;
+
+
+        SceneManager.LoadScene(SceneToLoad);
+
 	}
 }
